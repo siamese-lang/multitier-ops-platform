e8872f1bf0962216a73a1d3bcc072d91a2672963
siamese-lang <askj95@naver.com>
2026-07-12 02:23:37 +0900

    Add ops sample service context test

---
+++ apps/ops-sample-service/src/test/java/io/github/siamese_lang/ops/OpsSampleServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package io.github.siamese_lang.ops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpsSampleServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
