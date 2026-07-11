46454742e434ca093f05e204dd04cd035433b156
siamese-lang <askj95@naver.com>
2026-07-12 06:51:30 +0900

    Add lab-full-ops validation profile locals

--- infra/terraform/envs/lab-full-ops/locals.tf
+++ infra/terraform/envs/lab-full-ops/locals.tf
@@ -6,13 +6,27 @@ locals {
     data.aws_availability_zones.available.names[0]
   )
 
-  app_nodes = {
-    "app-01" = {
-      name = "app-01"
-    }
-    "app-02" = {
-      name = "app-02"
-    }
+  app_nodes = merge(
+    {
+      "app-01" = {
+        name = "app-01"
+      }
+    },
+    var.enable_app_02 ? {
+      "app-02" = {
+        name = "app-02"
+      }
+    } : {}
+  )
+
+  validation_profile = {
+    nat_gateway     = var.enable_nat_gateway
+    app_02          = var.enable_app_02
+    storage_node    = var.enable_storage_node
+    backup_node     = var.enable_backup_node
+    monitoring_node = var.enable_monitoring_node
+    logging_node    = var.enable_logging_node
+    loadgen_node    = var.enable_loadgen_node
   }
 
   common_tags = merge(
